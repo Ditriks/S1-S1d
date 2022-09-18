acf596ab7b1ca792c61715a0d0dde864030a90ac
Tatiana <tt_tanya_88@mail.ru>
2022-09-18 14:17:26 +0300

    Zadane3

---
+++ Zad3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int max = 0;
+
+if (N<0)
+{
+    count = N;
+    max = -N;
+}
+else
+{
+    count = N;
+    max = N;
+}
