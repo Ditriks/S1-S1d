2db69ea17ae7bc82a0fe1c7b64cbd7184f913841
Tatiana <tt_tanya_88@mail.ru>
2022-09-18 14:18:06 +0300

    Domash1

---
+++ Dz1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > y)
+    {
+        Console.WriteLine("Первое число больше второго");
+    }
+else
+    {
+        Console.WriteLine("Второе число больше первого");
+    }
