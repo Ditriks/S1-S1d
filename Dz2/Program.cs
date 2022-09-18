749babd90f24a6f9bc5c1cb41940aef6cee0f6f8
Tatiana <tt_tanya_88@mail.ru>
2022-09-18 14:18:18 +0300

    Domash2

---
+++ Dz2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+int max = x;
+
+if (y > max)
+    max=y;
+
+if (z > max)
+    max=z;
+
+Console.Write("Самое большое число: " + max);
+    
